1a02fa39e4e52de67ab89699dad9896182a69b8a
ct29oct <ct29oct@gmail.com>
2022-05-03 12:58:17 +0530

    Add some Repository

--- SpringBootCRUDOperations/src/main/java/com/demo/repository/EmployeeRepository.java
+++ SpringBootCRUDOperations/src/main/java/com/demo/repository/EmployeeRepository.java
@@ -1,5 +1,9 @@
 package com.demo.repository;
 
-public class EmployeeRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import com.demo.model.EmployeeModel;
+
+public interface EmployeeRepository extends CrudRepository<EmployeeModel,Integer> {
 
 }
